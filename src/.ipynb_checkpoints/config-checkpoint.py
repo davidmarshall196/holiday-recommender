026f4a4b20ccf7cc75d8bfd568657c70bfba9611
David Marshall <david@inawisdom.com@Davids-MacBook-Pro.local>
2023-09-13 10:08:24 +0100

    modified region

--- src/.ipynb_checkpoints/config-checkpoint.py
+++ src/.ipynb_checkpoints/config-checkpoint.py
@@ -1,9 +1,11 @@
 
 import boto3
+import os
 
 session = boto3.Session(
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name='eu-west-2'
 )
 
 def get_parameter(parameter_name):
