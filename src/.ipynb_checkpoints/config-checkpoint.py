db83dc32ec91122743a46b4edb35d8dad30ef2d1
David Marshall <david@inawisdom.com@Davids-MacBook-Pro.local>
2023-09-13 09:54:55 +0100

    modified boto3 session

--- src/.ipynb_checkpoints/config-checkpoint.py
+++ src/.ipynb_checkpoints/config-checkpoint.py
@@ -1,7 +1,10 @@
 
 import boto3
 
-session = boto3.Session(profile_name='david-gmail-acc')
+session = boto3.Session(
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
 
 def get_parameter(parameter_name):
     ssm_client = session.client('ssm')
